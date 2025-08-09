08b117ceca6024c7e021ac665c968f35bc27fc87
marianoluiz <kmarianu@gmail.com>
2025-08-10 06:55:00 +0800

    created mvn project template

---
+++ src/main/java/dev/marianoluiz/Main.java
@@ -0,0 +1,13 @@
+package dev.marianoluiz;
+
+/**
+ * Must Accept a File
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hi World!" );
+    }
+}
