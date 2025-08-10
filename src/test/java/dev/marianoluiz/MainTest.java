11393e2a7be09a65851667880f78d56db6b6cf49
marianoluiz <kmarianu@gmail.com>
2025-08-10 21:03:36 +0800

    updated test lib and rm -r --cached target/

--- src/test/java/dev/marianoluiz/MainTest.java
+++ src/test/java/dev/marianoluiz/MainTest.java
@@ -1,12 +1,12 @@
 package dev.marianoluiz;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
+public class MainTest 
 {
     /**
      * Rigorous Test :-)
