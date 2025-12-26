c2c093b95f3583b39bcafc811f3bf20e62618e94
marianoluiz <kmarianu@gmail.com>
2025-12-26 18:34:12 +0800

    test: refactored syntax tests

--- test/syntax/test_syntax.py
+++ test/syntax/test_syntax.py
@@ -2,6 +2,7 @@ import os
 import sys
 import csv
 import pytest
+import re
 
 # Add src as root
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
@@ -49,19 +50,17 @@ def _rows():
         next(reader)  # skip header
 
         for row in reader:
-            # row:  ['abc123 = 10', 'NO LEXICAL ERROR/S'] ...
-            src = row[1]                # The source code, col 1
-            expected_ast = row[2] 
-            expected_error = row[3].strip()   # Expected Error, col 2
+            # row:  ['abc123 = 10', 'NO SYNTAX ERROR'] ...
+            src = row[0]                # The source code, col 1
+            expected_error = row[1].strip()   # Expected Error, col 2
 
             # Normalize CRLFs and CR to LF if any, since spreadsheets do use CRLFs
-            expected_ast = expected_ast.replace("\r\n", "\n").replace("\r", "\n")
             expected_error = expected_error.replace("\r\n", "\n").replace("\r", "\n")
 
-            yield src, expected_ast, expected_error         # return one at a time
+            yield src, expected_error         # return one at a time
 
-@pytest.mark.parametrize("src,expected_ast,expected_error", _rows())
-def test_syntax(src, expected_ast, expected_error):
+@pytest.mark.parametrize("src,expected_error", _rows())
+def test_syntax(src, expected_error):
     """ the src, expected will be extracted from the _rows() at this parametrizing test """
     # pytest.mark.parametrize is LIKE a loop that runs testcase per testcase
     # _rows() returns a generator, which pytest automatically does the next()
@@ -69,29 +68,50 @@ def test_syntax(src, expected_ast, expected_error):
     lx = _run_lexer(src)
     
     assert lx.log.strip() == "", f"Lexer produced errors:\n{lx.log}"
-       
+
     # Build token dicts expected by RDParser
     tokens = lx.token_stream
-
 
     # Run parser
     parser = RDParser(tokens, src, debug=False)
     parse_result = parser.parse()
+    
+    actual_error = parse_result.errors[0] if parse_result.errors else '' # list to string
 
-    # convert string to Python object safely
-    actual_ast_str = ast_to_string(parse_result.tree)
-    expected_errors_list = [] if expected_error.lower() == "" else [expected_error]
+    if expected_error == "NO SYNTAX ERROR":
+        assert not actual_error, f"Expected no syntax errors, got: {actual_error}"
+    else:
 
-    # Debug: show source code
-    print("=== Source Code ===")
-    print(src)
-    print("=== Expected AST ===")
-    print(expected_ast)
-    print("=== Actual AST ===")
-    print(actual_ast_str)
+        # looks for the pattern "line <number> col <number>" anywhere in the string.
+        line_col_match = re.search(r"line (\d+) col (\d+)", expected_error)
 
-    # must match
-    assert expected_ast == actual_ast_str, \
-        f"\nExpected AST:\n{expected_ast}\n\nActual AST:\n{actual_ast_str}"
-    assert expected_errors_list == parse_result.errors, \
-        f"Expected errors: {expected_errors_list}\nActual errors: {parse_result.errors}"
+        if not line_col_match:
+            raise ValueError(f"Expected error string missing line/col info:\n{expected_error}")
+
+        ref_line = int(line_col_match.group(1))
+        ref_col = int(line_col_match.group(2))
+        
+        # Splits the string at "Expected any:" and takes the last part, which contains all tokens:
+        # Splits the last string at every comma, creating a list
+        ref_tokens = [tok.strip() for tok in expected_error.split("Expected any:")[-1].split(",")]
+
+        # looks for the pattern "line <number> col <number>" anywhere in the string.
+        line_col_match = re.search(r"line (\d+) col (\d+)", actual_error)
+        actual_line = int(line_col_match.group(1))
+        actual_col = int(line_col_match.group(2))
+
+        actual_tokens = [tok.strip() for tok in actual_error.split("Expected any:")[-1].split(",")]
+
+        # Debug prints
+        print("=== Source Code ===")
+        print(src)
+        print("=== Expected Syntax Error ===")
+        print(f'line {ref_line} col {ref_col}')
+        print(ref_tokens)
+        print("=== Actual Syntax Error ===")
+        print(actual_tokens)
+        print(f'line {actual_line} col {actual_col}')
+
+        assert ref_line == actual_line, f"Line mismatch: {ref_line} != {actual_line}"
+        assert ref_col == actual_col, f"Column mismatch: {ref_col} != {actual_col}"
+        assert ref_tokens == actual_tokens, f"Tokens mismatch: {ref_tokens} != {actual_tokens}"
