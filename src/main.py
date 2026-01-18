4d2916a525f8e6b9da990bbc358f725f0b5fb5fe
Luis Capiral <ihategmpls@gmail.com>
2026-01-18 17:41:15 +0800

    Update main.py
    feat: changed main to accommodate buttons

--- src/main.py
+++ src/main.py
@@ -4,18 +4,35 @@ from src.lexer.dfa_lexer import Lexer
 from src.syntax.syntax_test import Parser
 # from src.syntax.rd_parser import RDParser
 
-def lexer_adapter(source: str):
+
+def lexer_only_adapter(source: str):
     """
-    Adapter that runs the Lexer and converts its output into a list of dicts:
+    Adapter that runs only the Lexer and converts its output into a list of dicts:
       { "type": <token_type>, "lexeme": <lexeme>, "line": <1-based>, "col": <1-based> }
     and a list of error strings.
     """
-
-
-
     lexer = Lexer(source, debug=False)
     lexer.start()
-    
+
+    tokens = lexer.token_stream
+    errors = []
+
+    if lexer.log:
+        errors.append("Lexical Error/s:")
+        errors.extend(lexer.log.splitlines())
+
+    return tokens, errors
+
+
+def syntax_adapter(source: str):
+    """
+    Adapter that runs both the Lexer and Parser and converts its output into a list of dicts:
+      { "type": <token_type>, "lexeme": <lexeme>, "line": <1-based>, "col": <1-based> }
+    and a list of error strings.
+    """
+    lexer = Lexer(source, debug=False)
+    lexer.start()
+
     tokens = lexer.token_stream
     errors = []
 
@@ -42,7 +59,8 @@ def lexer_adapter(source: str):
 
     return tokens, errors
 
+
 if __name__ == "__main__":
     root = tk.Tk()
-    app = ChungusLexerGUI(root, lexer_callback=lexer_adapter)
+    app = ChungusLexerGUI(root, lexer_callback=lexer_only_adapter, syntax_callback=syntax_adapter)
     root.mainloop()
